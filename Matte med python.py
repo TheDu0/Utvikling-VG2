82b3debbcb3795c6f4297e21d73601039cf329ed
Max <maxknutsen123@hotmail.com>
2021-08-24 13:16:36 +0200

    Laget en ting som reger ut areal og omkrets

---
+++ Matte med python.py
@@ -0,0 +1,8 @@
+høyde = int(input("Skriv inn høyde: "))
+bredde = int(input("Skriv inn bredde: "))
+
+areal = høyde*bredde
+omkrets = 2*bredde + 2*høyde
+print("Arealet til objektet er: " + str(areal))
+print("Omkretsen er: " + str(omkrets))
+
