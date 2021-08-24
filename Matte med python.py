838d80618dbe016ef1e451e51a302c57a929f828
Max <maxknutsen123@hotmail.com>
2021-08-24 13:27:39 +0200

    Nå kan den regne ut arealet til en trekant

--- Matte med python.py
+++ Matte med python.py
@@ -6,3 +6,10 @@ omkrets = 2*bredde + 2*høyde
 print("Arealet til objektet er: " + str(areal))
 print("Omkretsen er: " + str(omkrets))
 
+#regner ut areal og omkrets av en trekant
+
+høyde = int(input("Skriv inn høyden til trekanten: "))
+grunnlinje = int(input("Skriv inn bredden: "))
+
+areal = høyde*grunnlinje/2
+print("Arealet til trekant er: " + str(areal))
